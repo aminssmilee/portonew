c50d258e0d1263422504532e9252f31138e2aaed
aminssmilee <salisahmad48@gmail.com>
2025-07-15 15:44:28 +0700

    Fix font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['"Roboto Slab"', 'serif'],
-        manrope: ['Manrope', 'sans-serif'],       
-        inter: ['Inter', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        lora: ['Lora', 'serif'],
-        playfair: ['"Playfair Display"', 'serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
-        fredoka: ['"Fredoka One"', 'cursive'],
+        fredoka: ["Fredoka", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        lora: ["Lora", "serif"],
+        playfair: ["Playfair Display", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
       },
       colors: {
-        jeruk: '#FF4500', // 🎯 Warna oranye kemerahan seperti jeruk matang
+        jeruk: "#FF4500", // 🎯 Warna oranye kemerahan seperti jeruk matang
       },
     },
   },
   plugins: [],
-}
+};
