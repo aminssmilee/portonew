ca13574fda5b11086e9b705c2b7c01c9a864a40a
aminssmilee <salisahmad48@gmail.com>
2025-08-01 11:09:02 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Scroll from "./pages/Scroll";
 import { Contact } from "lucide-react";
 import Contac from "./pages/Contact";
 import MoreAbout from "./pages/MoreAbout";
-import ScrollStack, { ScrollStackItem } from '../src/components/ScrollStack';
 
 function App() {
   return (
@@ -17,27 +16,7 @@ function App() {
         <Home />
         <MoreAbout />
         <Service />
-        <Project />
-        <div className="bg-white h-screen border-t border-gray-500">
-          <ScrollStack>
-            <ScrollStackItem>
-              <h2>Card 1</h2>
-              <p>This is the first card in the stack</p>
-            </ScrollStackItem>
-            <ScrollStackItem>
-              <h2>Card 2</h2>
-              <p>This is the second card in the stack</p>
-            </ScrollStackItem>
-            <ScrollStackItem>
-              <h2>Card 3</h2>
-              <p>This is the third card in the stack</p>
-            </ScrollStackItem>
-            <ScrollStackItem>
-              <h2>Card 4</h2>
-              <p>This is the fourth card in the stack</p>
-            </ScrollStackItem>
-          </ScrollStack>
-        </div>
+        <Project /> 
         <Scroll />
         <Contac />
       </div>
